d8cb8592da2d2c6ee6e4f030666955559ed17eb0
Carolina Simón <carolina.simon.guerrero@gmail.com>
2024-03-29 20:30:58 +0100

    Add swagger and adjust views.py to work with swagger

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_restx import Api
 from db import db
-from views import register_views
+from views import ns_project
 
-# Instanciate flask app
+# Instanciate flask app and Flask-RESTx Api
 app = Flask(__name__)
+api = Api(app, version='1.0', title='FandanGO iRODS API', description='iRODS microservice API within fandanGO application') 
 
 # Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fandango-irods.db' # URI configuration (sqlite and fandango.db)
@@ -16,7 +18,8 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-register_views(app)
+# Import and register namespaces
+api.add_namespace(ns_project, path='/projects')
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
--- views.py
+++ views.py
@@ -1,31 +1,37 @@
 from flask import request, jsonify
+from flask_restx import Namespace, Resource, fields
 from db import db
 from db import Project
 from utils import update_or_create_project
 
+ns_project = Namespace('projects', description='Projects operations')
 
-def register_views(app):
-    @app.route('/projects/', methods=['GET'])
-    def get_projects():
-            projects = Project.query.all()
-            projects_list = [project.to_dict() for project in projects]
-            return jsonify(projects_list)
+# Project Model for Swagger
+project_model = ns_project.model('Project', {
+    'id': fields.Integer(description='The project ID'),
+    'name': fields.String(required=True, description='The project name'),
+    'details': fields.String(required=True, description='The project details'),
+    'rawdata': fields.String(required=True, description='The project raw data')
+})
 
-    @app.route('/copydata/', methods=['POST'])
-    def post_copydata():
-            
-            # Get data from request
-            data = request.get_json()
 
-            project_id = data['id']
-            project_name = data['name']
-            project_details = data['details']
-            project_rawdata = data['rawdata']
+@ns_project.route('/')
+class ProjectList(Resource):
+    @ns_project.doc('list_projects')
+    @ns_project.marshal_list_with(project_model)
+    def get(self):
+        projects = Project.query.all()
+        return projects
 
-            # Update or create project
-            project = update_or_create_project(project_id, project_name, project_details, project_rawdata)
-
-            # DO WHATEVER
-            #... irods commands to create, given project_id and raw_data_path, a collection and get success boolean and irods info dict (containing ticket id) 
-
-            return jsonify({'message': 'Project created or updated successfully'}), 200
+@ns_project.route('/copydata/')
+class ProjectData(Resource):
+    @ns_project.doc('create_or_update_project')
+    @ns_project.expect(project_model, validate=True)
+    def post(self):
+        data = ns_project.payload
+        project_id = data['id']
+        project_name = data['name']
+        project_details = data['details']
+        project_rawdata = data['rawdata']
+        project = update_or_create_project(project_id, project_name, project_details, project_rawdata)
+        return {'message': 'Project created or updated successfully'}, 200
